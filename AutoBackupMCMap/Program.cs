c6f2c7c8ab4378059128b3c9c4a7bf7d2f67ef06
HuaWaterED <1758158706@qq.com>
2022-11-10 09:45:18 +0800

    没什么想说的，本来没有这玩意

---
+++ AutoBackupMCMap/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO.Compression;
+using System.Timers;
+
+namespace AutoBackupMCMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainActivity mainActivity = new();
+            mainActivity.Activity();
+            Console.ReadKey();
+        }
+    }
+    internal class MainActivity
+    {
+        string backupdic;
+        string zipPath;
+        uint backupTime;
+        public void Activity()
+        {
+            Console.Write("输入需要备份的文件夹：");
+            backupdic = Console.ReadLine();
+            Console.Write("输入存放备份文件的路径：");
+            zipPath = Console.ReadLine();
+            Console.Write("请输入每隔多少时间备份一次(单位：毫秒)：");
+            backupTime = uint.Parse(Console.ReadLine());
+
+            Timer timer = new(backupTime);
+
+            timer.Elapsed += Timer_Elapsed;
+
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(@$"{DateTime.Now}执行一次");
+            ZipFile.CreateFromDirectory(backupdic + "/", zipPath + @$"/mcpe_AutoBackupMap_{DateTime.Now.Year}_{DateTime.Now.Month}_{DateTime.Now.Day}_{DateTime.Now.Hour}_{DateTime.Now.Minute}_{DateTime.Now.Second}_{DateTime.Now.Millisecond}.zip", CompressionLevel.SmallestSize, false);
+        }
+    }
+}
